697f1a2ff57f8f4f316d76b1c70c237b4bf30561
KristinaPopovSV5-2020 <popov.sv5.2020@uns.ac.rs>
2023-11-10 14:39:19 +0100

    fixed DB and DL

--- DB/door_buzzer.py
+++ DB/door_buzzer.py
@@ -7,19 +7,24 @@ except:
     pass
 
 
-
-
-
-def buzz(pin,pitch, duration):
-    GPIO.setup(pin, GPIO.OUT)
+def buzz(pin, pitch, duration):
+    print("Sound on")
+    #GPIO.setup(pin, GPIO.OUT)
     period = 1.0 / pitch
     delay = period / 2
     cycles = int(duration * pitch)
     for i in range(cycles):
-        GPIO.output(pin, True)
+        #GPIO.output(pin, True)
         time.sleep(delay)
-        GPIO.output(pin, False)
+        #GPIO.output(pin,False)
         time.sleep(delay)
+
+def play_sound(pin):
+    buzz(pin, 440, 0.1)
+
+def stop_sound():
+    print("Sound off")
+
 
 
 
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from DL.door_light import turn_on, turn_off
 import keyboard
 from settings import load_settings
+import threading
+
+from DB.door_buzzer import play_sound,stop_sound
 
 
 try:
@@ -9,9 +12,17 @@ try:
 except:
     pass
 
+
+def handle_b_key(pin):
+    if keyboard.is_pressed('B'):
+        play_sound(pin)
+    if not keyboard.is_pressed('B'):
+        stop_sound()
+
 if __name__ == "__main__":
     settings = load_settings()
     threads = []
+    stop_event = threading.Event()
     while True:
         print("Choose an option:")
         print("1. Door sensor")
@@ -32,16 +43,17 @@ if __name__ == "__main__":
         elif option == "2":
             dl_settings = settings['DL']
             print("You have selected the door light option.")
-            print("If you want to turn on door light we need to press the keyboard button 'o',"
-                  "If you want to turn off door light we need to press the keyboard button 'x'")
-            keyboard.add_hotkey('o', turn_on(dl_settings['pin']))
-            keyboard.add_hotkey('x', turn_off(dl_settings['pin']))
+            print("If you want to turn on door light we need to press the keyboard button 'O'\n"
+                  "If you want to turn off door light we need to press the keyboard button 'X'\n")
+            keyboard.add_hotkey('o', lambda:turn_on(dl_settings['pin']))
+            keyboard.add_hotkey('x', lambda:turn_off(dl_settings['pin']))
         elif option == "3":
             print("You have selected the door ultrasonic sensor option.")
         elif option == "4":
-            db_settings = settings['DL']
+            db_settings = settings['DB']
             print("You have selected the door buzzer option.")
-
+            print("if you want to hear the sound, we need to hold the keyboard button 'B' \n")
+            keyboard.add_hotkey('B', lambda:handle_b_key(db_settings["pin"]))
         elif option == "5":
             print("You have selected the door motion sensor option.")
         elif option == "6":
@@ -55,6 +67,7 @@ if __name__ == "__main__":
         elif option == "9":
             print("You have exited the program.")
             GPIO.cleanup()
+            keyboard.unhook_all()
             break
         else:
             print("Invalid input.")
