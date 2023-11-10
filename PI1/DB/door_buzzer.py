54631cfca3c12ca14801da0856b9a9fb11a0ecd2
KristinaPopovSV5-2020 <popov.sv5.2020@uns.ac.rs>
2023-11-10 12:50:01 +0100

    added dht

--- PI1/DB/door_buzzer.py
+++ PI1/DB/door_buzzer.py
@@ -8,18 +8,17 @@ except:
 
 
 
-def setup_gpio():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setup(BUZZER_PIN, GPIO.OUT)
 
-def buzz(pitch, duration):
+
+def buzz(pin,pitch, duration):
+    GPIO.setup(pin, GPIO.OUT)
     period = 1.0 / pitch
     delay = period / 2
     cycles = int(duration * pitch)
     for i in range(cycles):
-        #GPIO.output(BUZZER_PIN, True)
+        GPIO.output(pin, True)
         time.sleep(delay)
-        #GPIO.output(BUZZER_PIN, False)
+        GPIO.output(pin, False)
         time.sleep(delay)
 
 
