fadb545738da11e8cfb00a74409a9639988fc02d
slepimis120 <slepimis120@gmail.com>
2024-01-23 21:09:30 +0100

    feat: Implement Sockets

--- SmartHomeSimulator/devices/simulators/pir.py
+++ SmartHomeSimulator/devices/simulators/pir.py
@@ -4,7 +4,7 @@ import random
 
 def generate_motion():
     while True:
-        yield True
+        yield random.choice([True, False])
 
 
 def run_pir_simulator(delay, callback, stop_event, publish_event, settings):
